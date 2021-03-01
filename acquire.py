b49ec36bfb2f801a624054a50ec8bcd9f5b879fb
Samuel Davila <samuelrdavila@gmail.com>
2021-03-01 15:44:42 -0600

    updated doc string in sole function

--- acquire.py
+++ acquire.py
@@ -7,7 +7,9 @@ warnings.filterwarnings("ignore")
 
 def get_hearth():
     """
-    No argument needed. Acquires data and returns as DFs, 8 in total.
+    No argument needed. Acquires data and returns as DFs, 7 in total.
+    DFs will be returned in the following order:
+    cards, classes, mtypes, rarities, sets, ctypes, keywords
     """
 
     # reading in card data and saving as DF
